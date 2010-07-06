660d868ddaaedff1166f2940855b0bc5deea3ffd
Phanx-28751 <Phanx-28751@5d13539c-25a5-4bc8-b52a-5474e3ee8778>
2010-07-06 02:41:29 +0000

    - Add svn:keywords "Revision" for automatic minor version incrementing

--- trunk/PhanxConfig-ColorPicker/PhanxConfig-ColorPicker.lua
+++ trunk/PhanxConfig-ColorPicker/PhanxConfig-ColorPicker.lua
@@ -9,7 +9,9 @@
 	its internals may change at any time without notice.
 ----------------------------------------------------------------------]]
 
-local lib, oldminor = LibStub:NewLibrary("PhanxConfig-ColorPicker", 3)
+local MINOR_VERSION = tonumber(("$Revision$"):match("%d+"))
+
+local lib, oldminor = LibStub:NewLibrary("PhanxConfig-ColorPicker", MINOR_VERSION)
 if not lib then return end
 
 local function OnEnter(self)
